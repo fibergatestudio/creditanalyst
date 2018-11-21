fdca27a94d6f1fa0eae8ec76cf458a71721dfd8b
Vyacheslav <vyacheslavfil@ukr.net>
2018-11-21 13:57:04 +0200

    charts

--- app/Http/Controllers/Admin/ChartsController.php
+++ app/Http/Controllers/Admin/ChartsController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Admin\AdminController;
+use DateTime;
+use DateInterval;
 
 class ChartsController extends AdminController
 {
 	public function index(){
 		$title = 'Графики';
+		$days_of_year = [];
+		$aDates = array();
+		$oStart = new DateTime('2018-01-01');
+		$oEnd = clone $oStart;
+		$oEnd->add(new DateInterval("P1Y"));
+
+		while ($oStart->getTimestamp() < $oEnd->getTimestamp()) {
+			$aDates[] = $oStart->format('d');
+			$oStart->add(new DateInterval("P1D"));
+		}
+		//echo '<pre>'.print_r($aDates,true).'</pre>';
+
 		return view('admin.charts', ['title' => $title]);
 	}
 
