4ea19369a8ab2334bd25ffb3e6466086c32d82f9
Askariot <magicjoke@gmail.com>
2019-01-16 16:40:55 +0200

    16.01 #1

--- resources/views/monitoring/index.blade.php
+++ resources/views/monitoring/index.blade.php
@@ -5,10 +5,8 @@
 <script src="{{ asset('js/app.js') }}" defer></script>
 <div class="container" id="app">
     <div class="row">
-        <div class="col col-lg-3" style="padding-left: 0px; padding-top: 15px;">
+        <div class="col col-lg-12" style="padding-left: 0px; padding-top: 15px;">
         <user-indicator-list />
-        </div>
-        <div class="col col-lg-9">
         </div>
     </div>
 </div>
